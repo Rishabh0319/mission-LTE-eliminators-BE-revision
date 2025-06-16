a248c07b237c9dc900f98e3a5be06c16a5246c54
Rishabh0319 <rishabhtripathi0319@gmail.com>
2025-06-16 12:09:19 +0530

    [feat]:[custom-API-response]: API req, res standardization and error handeling

---
+++ custom-API-response-and-error-handling/src/utils/asyncHandler.js
@@ -0,0 +1,28 @@
+// API standard for handling async operations in Express.js
+
+
+// by using Promises
+const asyncHandler = (requestHandler) => {
+    (req, res, next) => {
+        Promise.resolve(requestHandler(req, res, next)).catch((error) => next(error))
+    }
+}
+
+
+
+
+
+// another way by using try catch
+
+// const asyncHandler = (fn) => async (req, res, next) => {
+//     try {
+//         await fn(req, res, next);
+//     } catch (error) {
+//         res.status(error.code || 500).json({
+//             success: false,
+//             message: error.message
+//         });
+//     }
+// }
+
+// export default asyncHandler;
