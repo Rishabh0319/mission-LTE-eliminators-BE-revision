2ce8c3623b8b080fcb91cf230ea1ff0529f37c86
Rishabh0319 <rishabhtripathi0319@gmail.com>
2025-06-26 18:26:30 +0530

    [feat]:[authentication-and-authorization] how to creat and read JWT token

--- authentication-and-authorization/src/script.js
+++ authentication-and-authorization/src/script.js
@@ -1,7 +1,7 @@
 import express from "express";
 import cookieParser from "cookie-parser";
 import bcrypt from "bcrypt";
-
+import jwt from "jsonwebtoken";
 const app = express();
 const PORT = 3000;
 
@@ -27,30 +27,48 @@ app.use(cookieParser());
 
 
 
-// encrypt password
+// // encrypt password
+
+// app.get("/", (req, res) => {
+
+//     bcrypt.genSalt(10, function (error, salt) {
+//         bcrypt.hash("my-password", salt, function (error, hash) {
+//             console.log(hash);
+//         })
+//     })
+
+//     res.send("all set");
+// })
+
+
+// // checking encrepted password and your original password is same or not
+// app.get("/pass", (req, res) => {
+
+//     bcrypt.compare("my-password", "$2b$10$xRgP3Iy5HDprdWp1f6Vsfu.6lismXsBn3STXGYbHRI3jGbq6KdpUu", function (error, result) {
+//         console.log(result);
+//     })
+
+//     res.send("checking password", res);
+// })
+
+
+
+
+// how to create and store JWT token and read jwt token
 
 app.get("/", (req, res) => {
-
-    bcrypt.genSalt(10, function (error, salt) {
-        bcrypt.hash("my-password", salt, function (error, hash) {
-            console.log(hash);
-        })
-    })
-
-    res.send("all set");
-})
+    let token = jwt.sign({ email: "rishabhtripathi@gmail.com" }, "secret");
+    res.cookie("token", token);
+    res.send("Cookie is stored");
+});
 
 
-// checking encrepted password and your original password is same or not
-app.get("/pass", (req, res) => {
-
-    bcrypt.compare("my-password", "$2b$10$xRgP3Iy5HDprdWp1f6Vsfu.6lismXsBn3STXGYbHRI3jGbq6KdpUu", function (error, result) {
-        console.log(result);
-    })
-
-    res.send("checking password", res);
-})
-
+// read jwt token by decrept the token
+app.get("/read", (req, res) => {
+    let data = jwt.verify(req.cookies.token, "secret");
+    console.log(data);
+    res.send("token decrepted");
+});
 
 
 app.listen(PORT, () => {
