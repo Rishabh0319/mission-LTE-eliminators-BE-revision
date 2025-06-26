34c572769d0e7a389864a417c7d4fc3d2da60322
Rishabh0319 <rishabhtripathi0319@gmail.com>
2025-06-26 17:38:45 +0530

    [feat]:[authentication-and-authorization] how to encript and compare password to check password is correct or not

--- authentication-and-authorization/src/script.js
+++ authentication-and-authorization/src/script.js
@@ -1,5 +1,6 @@
 import express from "express";
 import cookieParser from "cookie-parser";
+import bcrypt from "bcrypt";
 
 const app = express();
 const PORT = 3000;
@@ -9,16 +10,47 @@ app.use(cookieParser());
 
 
 // set Cookie                 (cookie store krana user browser pe)
+// app.get("/", (req, res) => {
+//     res.cookie("name", "Rishabh Tripathi");
+//     res.send("Cookie is set");
+// });
+
+
+// app.get("/read", (req, res) => {
+//     console.log(req.cookies);
+//     res.send("read cookie");
+// })
+
+
+
+
+
+
+
+// encrypt password
+
 app.get("/", (req, res) => {
-    res.cookie("name", "Rishabh Tripathi");
-    res.send("Cookie is set");
-});
 
+    bcrypt.genSalt(10, function (error, salt) {
+        bcrypt.hash("my-password", salt, function (error, hash) {
+            console.log(hash);
+        })
+    })
 
-app.get("/read", (req, res) => {
-    console.log(req.cookies);
-    res.send("read cookie");
+    res.send("all set");
 })
+
+
+// checking encrepted password and your original password is same or not
+app.get("/pass", (req, res) => {
+
+    bcrypt.compare("my-password", "$2b$10$xRgP3Iy5HDprdWp1f6Vsfu.6lismXsBn3STXGYbHRI3jGbq6KdpUu", function (error, result) {
+        console.log(result);
+    })
+
+    res.send("checking password", res);
+})
+
 
 
 app.listen(PORT, () => {
